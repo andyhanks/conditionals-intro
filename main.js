45f0bb5d894d4ed91ae1fa9c50b3143b8c7a2c52
Andrew Hanks <andymchanks@gmail.com>
2023-08-30 11:13:47 -0400

    First solo commit. conditionals-intro

---
+++ main.js
@@ -0,0 +1,59 @@
+// Check if 1+1 >5
+/*if(1+1 > 5) {
+    console.log("True Facts!")
+}
+else {
+    console.log("Lies!")
+}*/
+// Should print lies
+
+/*let likesDogs = true;
+
+if(likesDogs) {
+    console.log("You're a dog person!")
+}
+else {
+    console.log("You're a cat person!")
+}*/
+// Should print "You're a dog person!"
+
+// An object representing a person
+/*var person = {
+    name: "Andy",
+    prefLang: "French"
+}
+
+if(person.prefLang === "English") {
+console.log(`Hello, ${person.name}!`)
+}
+    else if(person.prefLang === "Spanish") {
+       console.log(`Hola, ${person.name}!`)
+    }
+    else if(person.prefLang === "French") {
+        console.log(`Bonjour, ${person.name}!`)
+     }
+ else {
+    console.log("Hello.")
+ } */
+ //Checks preferred language and prints a greeting!
+
+ let grade = 72;
+
+ if(grade >= 0 && grade <= 69) {
+console.log("You got an F")
+ }
+ else if(grade > 70 && grade < 76) {
+    console.log("You got a D")
+ }
+
+ else if(grade > 77 && grade < 84) {
+    console.log("You got a C")
+ }
+
+ else if(grade > 84 && grade < 92) {
+    console.log("You got a B")
+ }
+
+ else if(grade > 93 && grade <= 100) {
+    console.log("You got an A")
+ }
